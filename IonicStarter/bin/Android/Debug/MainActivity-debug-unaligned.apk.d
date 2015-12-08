3a4f48bd8817eae5ebd812a74f4391c323712f08
Quintin Denman <denmanq@outlook.com>
2015-12-08 12:27:09 -0600

    Added an embedded video to the controller page

--- IonicStarter/bin/Android/Debug/MainActivity-debug-unaligned.apk.d
+++ IonicStarter/bin/Android/Debug/MainActivity-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-C:\Users\Miguel\Source\Repos\controller\IonicStarter\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
-C:\Users\Miguel\Source\Repos\controller\IonicStarter\platforms\android\ant-build\MainActivity.ap_ \
-C:\Users\Miguel\Source\Repos\controller\IonicStarter\platforms\android\ant-build\classes.dex \
-C:\Users\Miguel\Source\Repos\controller\IonicStarter\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\Users\denma\Documents\GitHubVisualStudio\controller\IonicStarter\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
+C:\Users\denma\Documents\GitHubVisualStudio\controller\IonicStarter\platforms\android\ant-build\MainActivity.ap_ \
+C:\Users\denma\Documents\GitHubVisualStudio\controller\IonicStarter\platforms\android\ant-build\classes.dex \
+C:\Users\denma\Documents\GitHubVisualStudio\controller\IonicStarter\platforms\android\CordovaLib\ant-build\classes.jar \
