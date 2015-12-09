73171c014d7311430aef80ef5ee67ddf84722e05
Quintin Denman <denmanq@outlook.com>
2015-12-09 01:55:02 -0600

    Moved the bluetooth connection to the settings tab and fixed the video portion

--- IonicStarter/www/js/controlCtrl.js
+++ IonicStarter/www/js/controlCtrl.js
@@ -35,30 +35,6 @@
         $scope.pressed = false;
     }
 
-    $scope.btScan = function () {
-        $scope.spinner = true;
-        BT.scan(function () {
-            $scope.$broadcast('scroll.refreshComplete');
-            $scope.stale = false;
-        })
-    };
-
-    $scope.btConnect = function (deviceId) {
-        $scope.spinner = true;
-        BT.connect(
-            deviceId,
-            function (val) { 
-                console.log("con val = " + val); 
-                $scope.connected = val; 
-                $scope.spinner = !val; 
-                if (!$scope.$$phase) { $scope.$apply() }
-            }
-        );
-    };
-    $scope.btDisconnect = function (deviceId) {
-        $scope.spinner = true;
-        BT.disconnect(function (val) { console.log("disc val = " + val); $scope.connected = val; $scope.spinner = val; if (!val) { $scope.$apply() } });
-    };
     $scope.btWrite = function (value) {
         BT.write(value);
     };
